fc8dfe0be0e8638aaec18f7861beb30a5e94f482
Jeffrey James Howarth <jeffhowarth31@gmail.com>
2018-08-10 13:05:56 -0700

    S2 Exercises

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,7 +10,12 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
         public ActionResult Random()
         {
             var movie = new Movie { Name = "Shrek!" };
@@ -28,6 +33,15 @@ namespace Vidly.Controllers
             };
 
             return View(viewModel);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
         }
     }
 }
