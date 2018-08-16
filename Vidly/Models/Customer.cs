84b2f0b56df36c8b4d4f6efff54850cf2a0780dc
Jeffrey James Howarth <jeff_howarth@yahoo.com>
2018-08-16 16:16:42 -0700

    S5 Data Annotations

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,8 +9,8 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
-
-        [Required]
+        
+        [Required(ErrorMessage = "Please enter customer's name.")]
         [StringLength(255)]
         public string Name { get; set; }
 
