a7263ed7b5c6f80df3716080c672aa6463e96c7b
Jeffrey James Howarth <jeff_howarth@yahoo.com>
2018-08-17 15:55:07 -0700

    S5 Validation Summary

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -23,14 +23,15 @@ namespace Vidly.Controllers
             _context.Dispose();
         }
 
-        public ActionResult CustomerForm()
+        public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
         }
 
         [HttpPost]
